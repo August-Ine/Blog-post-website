4efbc868bae537bbd6d9932b55a485e300687430
august269 <akirui95@gmail.com>
2023-03-13 14:10:31 +0300

    add internal js to truncate paragraph
    internal js added to home.ejs

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-//jshint esversion:6
-
+//imports
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { application } = require("express");
+const _ = require('lodash');
+
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -23,13 +24,15 @@ let posts = []; //empty array to store post objects
 app.get("/", (req, res) => {
   res.render("home", { homeContent: homeStartingContent, posts: posts }); //render ejs template with content
 });
+
 //route for post page
 app.get("/posts/:postName", (req, res) => {
   posts.forEach((post) => { //loop through all the objects in posts array
-    if (post.postTitle === req.params.postName) {
-      console.log("match found");
+    if (_.lowerCase(post.postTitle) === _.lowerCase(req.params.postName)) {
+      res.render("post", { post: post });
     } else {
       console.log("match not found");
+      res.send("404");
     };
   });
 });
